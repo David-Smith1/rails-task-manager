e662086c669da2e1b1ed841954d13ca08c44f5d4
David Smith <smitty6000@gmail.com>
2021-01-09 11:15:22 +0900

    index page done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+    Task.create(
+        title:Faker::String.random,
+        details:Faker::String.random,
+        completed: false
+    )
+end
